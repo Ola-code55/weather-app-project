43a6a07917de21e0c4911fa4b4d269f1aa41d21a
Olakusibe <125964202+Ola-code55@users.noreply.github.com>
2023-04-28 13:49:35 +0100

    added unit conversion

--- src/index.js
+++ src/index.js
@@ -60,12 +60,17 @@ function handleSubmit(event) {
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classLink.remove("active");
+  fahrenheitLink.classLink.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature); 
 }
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
+  celsiusLink.classLink.add("active");
+  fahrenheitLink.classLink.remove("active");
 let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
