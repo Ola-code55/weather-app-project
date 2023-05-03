8533692fdb8ba7852774b4bcb6f623de40ef6324
Olakusibe <125964202+Ola-code55@users.noreply.github.com>
2023-05-03 15:50:04 +0100

    Working on the forecast JavaScript

--- src/index.js
+++ src/index.js
@@ -21,7 +21,35 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-  
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  let days = ["Wed", "Thur", "Fri", "Sat"];
+  days.forEach(function(day) {
+ forecastHTML =  forecastHTML + `
+              <div class="col-2">
+                <div class="weather-forecast date">${day}</div>
+                <img
+                  src="https://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max">31° </span>
+                  <span class="weather-forecast-temperature-min">25° </span>
+                </div>
+              </div>
+            </div>
+           `;
+           })
+ 
+          forecast = forecastHTML + `</div>`;
+           forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -87,3 +115,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click" , displayCelsiusTemperature);
 
 search("Lagos");
+displayForecast();
