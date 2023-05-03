6c1353975bb6b88cf6eea19510ca87bb35bb8bee
Olakusibe <125964202+Ola-code55@users.noreply.github.com>
2023-05-03 19:42:20 +0100

    Display the forecast data using the API

--- src/index.js
+++ src/index.js
@@ -22,23 +22,32 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class ="row">`;
-
-  let days = ["Wed", "Thur", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML = forecastHTML + `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+   forecastHTML = forecastHTML + `
       <div class="col-2">
-        <div class="weather-forecast date">${day}</div>
-        <img src="https://openweathermap.org/img/wn/50d@2x.png" alt="" width="42" />
+        <div class="weather-forecast date">${formatDay(forecastDay.dt)}</div>
+        <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42" />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max">31° </span>
-          <span class="weather-forecast-temperature-min">25° </span>
+          <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+          <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
         </div>
       </div>
     `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -46,6 +55,10 @@ function displayForecast() {
 }
 function getForecast(coordinates) {
   console.log(coordinates);
+  let apiKey ="8161b4309ee03faae957729ba7104797";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -73,7 +86,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "9059385bd64583ed2218072dfccd53f7";
+  let apiKey = "8161b4309ee03faae957729ba7104797";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -115,4 +128,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click" , displayCelsiusTemperature);
 
 search("Lagos");
-displayForecast();
